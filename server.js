a29eff3dc017da934aa72fa09aee4916f4e1faf3
omarfarukee <umarfaruque1920@gmail.com>
2025-01-01 22:11:56 +0600

    ...

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const { connectDB } = require('./src/config/database');
 
 const app = express();
-const PORT = 5001;
+const PORT = process.env.PORT || 5001;
 
 // Middleware
 app.use(cors());
