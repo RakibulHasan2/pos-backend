d35a43626298233c82c6706f13ca750eac126efd
omarfarukee <umarfaruque1920@gmail.com>
2025-01-01 22:36:40 +0600

    sever error issue is not an issue we just waste our time on that haha

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const { connectDB } = require('./src/config/database');
 
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
