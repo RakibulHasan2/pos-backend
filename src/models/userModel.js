a132f087429ddc36ed551297c342854ce493127d
omarfarukee <umarfaruque1920@gmail.com>
2025-01-03 09:56:19 +0600

    ...

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -22,7 +22,7 @@ const userSchema = new mongoose.Schema({
     },
     role: {
         type: String,
-        enum: ['stuff', 'manager'],
+        enum: ['staff', 'manager'],
         required: true,
     },
 }, { timestamps: true });
