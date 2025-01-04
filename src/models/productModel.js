eefd06d7d26f93cd3aa4e079e9ee4ade97b1f332
omarfarukee <umarfaruque1920@gmail.com>
2025-01-04 11:28:48 +0600

    products CRUD done

---
+++ src/models/productModel.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+// Define the product schema
+const productSchema = new mongoose.Schema({
+    p_name: { type: String, required: true },
+    p_category: { type: String, required: true },
+    p_brand: { type: String, required: true },
+    p_code: { type: String, default: () => Math.floor(10000000 + Math.random() * 90000000).toString(), unique: true },
+    p_quantity: { type: Number, required: true },
+    p_price: { type: Number, required: true },
+    p_cost: { type: Number, required: true },
+    p_images: { type: String, default: "" }, // Array of image URLs
+    p_unit: { type: String, required: true },
+    tax: { type: Number, required: true }, // Percentage value
+    tax_method: { type: String, enum: ['inclusive', 'exclusive'], default: 'exclusive' },
+    p_details: { type: String, default: '' },
+}, { timestamps: true });
+
+module.exports = mongoose.model('Products', productSchema);
