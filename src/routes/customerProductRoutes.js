5dffacd0a69b87ba6564d47e04337e292da2c1d5
omarfarukee <umarfaruque1920@gmail.com>
2025-01-11 17:55:29 +0600

    ...

--- src/routes/customerProductRoutes.js
+++ src/routes/customerProductRoutes.js
@@ -1,7 +1,7 @@
 // routes/customerProductRoutes.js
 const express = require("express");
 const router = express.Router();
-const { createCustomerProduct,getAllCustomerProducts,getSingleCustomerProduct,updateCustomerProduct } = require("../../src/controllers/customerProductController");
+const { createCustomerProduct,getAllCustomerProducts,getSingleCustomerProduct,updateCustomerProduct, deleteCustomerProduct } = require("../../src/controllers/customerProductController");
 
 router.post("/createCustomerProduct", createCustomerProduct);
 // Route for fetching all customer products
@@ -9,4 +9,5 @@ router.get("/getAllCustomerProducts", getAllCustomerProducts);
 
 router.get("/single/:id", getSingleCustomerProduct);
 router.put("/update/:id", updateCustomerProduct );
+router.delete("/delete/:id", deleteCustomerProduct );
 module.exports = router;
